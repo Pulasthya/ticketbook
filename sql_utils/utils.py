ae56d94ebd42e173223d709b85c428a2bb41cbf9
Pulasthya Reddy <pulasthyareddy@gmail.com>
2022-06-22 20:45:17 +0530

    added function for generating uuid

--- sql_utils/utils.py
+++ sql_utils/utils.py
@@ -1,3 +1,5 @@
+import uuid
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -11,3 +13,6 @@ Session = sessionmaker(bind=engine)
 def get_session():
     session = Session()
     return session
+
+def generate_uuid():
+    return str(uuid.uuid4())
