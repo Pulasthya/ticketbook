a00963b20c76416d6255f9a2c8768158d1b479b8
Pulasthya Reddy <pulasthyareddy@gmail.com>
2022-06-22 21:19:32 +0530

    update in api name

--- user_api/__init__.py
+++ user_api/__init__.py
@@ -2,7 +2,7 @@ from user_api.apis import *
 
 api_routes = [
     ("/api/get/movies", get_movies, ["GET"]),
-    ("/api/get/screeings", get_screening, ["POST"]),
+    ("/api/get/screenings", get_screening, ["POST"]),
     ("/api/make/reservation", make_reservation, ["POST"]),
     ("/api/get/reservation", get_reservations, ["GET"]),
     ("/api/signup", sign_up, ["POST"]),
